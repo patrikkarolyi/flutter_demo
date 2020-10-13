fbc13a6a678c7e2f3aebb7adb822c15c67d2b326
Károlyi Patrik <patrikkarolyi@hotmail.com>
2020-10-13 10:20:38 +0200

    Redesign whole list view

--- lib/pages/ListPage.dart
+++ lib/pages/ListPage.dart
@@ -23,20 +23,65 @@ class _ListPageState extends State<ListPage> {
   @override
   void initState() {
     super.initState();
-    response = Remote.fetchMovies();
+    response = Remote.fetchPopularMovies();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: _getMoviesAppBar(),
-      body: FutureBuilder<Response>(
-          future: response,
-          builder: (context, snapshot) {
-            return snapshot.hasData
-                ? _getMoviesView(snapshot.data.results)
-                : _getEmptyMoviesView();
-          }),
+      body: NestedScrollView(
+        headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+          return [
+            SliverAppBar(
+              expandedHeight: 200.0,
+              floating: false,
+              pinned: true,
+              actions: [
+                IconButton(
+                  icon: Icon(Icons.star),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => FavoritePage()),
+                    );
+                  },
+                ),
+                IconButton(
+                  icon: Icon(Icons.sort),
+                  onPressed: () {
+                    _openSortPopup(context);
+                  },
+                ),
+              ],
+              flexibleSpace: FlexibleSpaceBar(
+                  centerTitle: true,
+                  title: Align(
+                    alignment: Alignment.bottomCenter,
+                    child: Card(
+                      color: Colors.black,
+                      elevation: 5,
+                      child: Text("Movies Flutter Application",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 16.0,
+                          )),
+                    ),
+                  ),
+                  background: Image.asset(
+                    "assets/uni.jpg",
+                    fit: BoxFit.cover,
+                  )),
+            ),
+          ];
+        },
+        body: FutureBuilder<Response>(
+            future: response,
+            builder: (context, snapshot) {
+              return snapshot.hasData
+                  ? _getMoviesView(snapshot.data.results)
+                  : _getEmptyMoviesView();
+            }),
+      ),
     );
   }
 
@@ -66,21 +111,26 @@ class _ListPageState extends State<ListPage> {
         });
   }
 
-  GridView _getMoviesView(List<Movie> movies) {
-    return GridView.builder(
-        itemCount: movies.length,
-        physics: BouncingScrollPhysics(),
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2),
-        itemBuilder: (BuildContext context, int index) {
-          Movie movie = movies[index];
-          return MovieDetailsWidget(
-            id: movie.id,
-            rating: movie.vote_average,
-            title: movie.title,
-            imageUrl: Remote.image_url + movie.poster_path,
-          );
-        });
+  Widget _getMoviesView(List<Movie> movies) {
+    return Container(
+      color: Colors.black,
+      child: GridView.builder(
+          itemCount: movies.length,
+          physics: BouncingScrollPhysics(),
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            childAspectRatio: 0.6,
+          ),
+          itemBuilder: (BuildContext context, int index) {
+            Movie movie = movies[index];
+            return MovieDetailsWidget(
+              id: movie.id,
+              rating: movie.vote_average,
+              title: movie.title,
+              imageUrl: Remote.image_url_small + movie.poster_path,
+            );
+          }),
+    );
   }
 
   Center _getEmptyMoviesView() {
@@ -91,7 +141,8 @@ class _ListPageState extends State<ListPage> {
 
   AppBar _getMoviesAppBar() {
     return AppBar(
-      title: Text("App name"),
+      title: Text("Hello movies!"),
+      backgroundColor: Colors.red,
       actions: [
         IconButton(
           icon: Icon(Icons.star),
